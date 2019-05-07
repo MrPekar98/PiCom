6da684596979c09609ef0186d0dd17c9fded41fb
Martin Pekár <32843490+MrPekar98@users.noreply.github.com>
2019-05-07 20:21:00 +0200

    Added parsing and conversion of pi_command.

--- src/pi_controller.h
+++ src/pi_controller.h
@@ -1,5 +1,9 @@
 #include <string.h>
-#define DATA 30
+#define DATA_LEN 30
+#define TO_NUM 48
+#define COM_1 0
+#define COM_2 2
+#define DATA 4
 
 // Enum to specify commands.
 // For example, 0;5;2;1;Hello, world.
@@ -12,11 +16,12 @@ typedef struct
 {
   enum level1 com1;
   enum level2 com2;
-  char data[DATA];
+  char data[DATA_LEN];
 } pi_command;
 
-// Prototype
+// Prototypes
 pi_command parse_picommand(char *command);
+char *tostring(pi_command com);
 
 // Menu for executing command.
 void pi_menu(pi_command command)
@@ -27,5 +32,18 @@ void pi_menu(pi_command command)
 // Parses string to pi_command. Example: "2;6;1;4;Hello, world"
 pi_command parse_picommand(char *command)
 {
-  return (pi_command) {.com1 = CD};
+  const size_t str_length = strlen(command);
+  unsigned i;
+  pi_command com = {.level1 = command[COM_1] - TO_NUM, .level2 = command[COM_2] - TO_NUM};
+
+  sprintf(com.data, "%s", command + DATA);
+  return com;
+}
+
+// Converts pi_command into string.
+char *tostring(pi_command com)
+{
+  char result[DATA_LEN + 5];
+  sprintf(result, "%d;%d;%s", com.level1, com.level2, com.data);
+  return result;
 }

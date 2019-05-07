298bdc2c9e0e5c5ee31ff265090f80d809edeb49
Martin Pekár <32843490+MrPekar98@users.noreply.github.com>
2019-05-07 21:03:41 +0200

    Removed redundant command level.

--- src/pi_controller.h
+++ src/pi_controller.h
@@ -1,21 +1,16 @@
 #include <string.h>
-#define DATA_LEN 30
+#define DATA_LEN 50
 #define TO_NUM 48
-#define COM_1 0
-#define COM_2 2
-#define DATA 4
+#define COM 0
+#define DATA 2
 
-// Enum to specify commands.
-// For example, 0;5;2;1;Hello, world.
-// Say, the above example creates a file with a name.
-enum level1 {CD = 1};
-enum level2 {EXEC = 1, MKDIR, RMDIR, TOUCH, RM};
+// Commands
+enum command {EXEC = 1, MKDIR, RMDIR, TOUCH, RM, RAW};
 
 // Structure representing command passed through a network.
 typedef struct
 {
-  enum level1 com1;
-  enum level2 com2;
+  enum command com;
   char data[DATA_LEN];
 } pi_command;
 
@@ -24,17 +19,17 @@ pi_command parse_picommand(char *command);
 char *tostring(pi_command com);
 
 // Menu for executing command.
-void pi_menu(pi_command command)
+int pi_menu(pi_command command)
 {
 
 }
 
-// Parses string to pi_command. Example: "2;6;1;4;Hello, world"
+// Parses string to pi_command. Example: "2;Hello, world"
 pi_command parse_picommand(char *command)
 {
   const size_t str_length = strlen(command);
   unsigned i;
-  pi_command com = {.level1 = command[COM_1] - TO_NUM, .level2 = command[COM_2] - TO_NUM};
+  pi_command com = {.com = command[COM] - TO_NUM};
 
   sprintf(com.data, "%s", command + DATA);
   return com;
@@ -43,7 +38,7 @@ pi_command parse_picommand(char *command)
 // Converts pi_command into string.
 char *tostring(pi_command com)
 {
-  char result[DATA_LEN + 5];
-  sprintf(result, "%d;%d;%s", com.level1, com.level2, com.data);
+  static char result[DATA_LEN + 3];
+  sprintf(result, "%d;%s", com.com, com.data);
   return result;
 }

02fc0be96d5fcf561e06211d6cb766613f6cf42d
Martin Pekár <32843490+MrPekar98@users.noreply.github.com>
2019-05-10 21:21:39 +0200

    Changed field name.

--- src/server.h
+++ src/server.h
@@ -1,9 +1,11 @@
 #include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
 #define BUFFER 1024
 
 typedef struct
 {
   short error;
   int sockfd;
-  struct sockaddr_in socket;
+  struct sockaddr_in s_addr;
 } server_con;

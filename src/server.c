30b5aa3ccc4163b935b9dace8115cb1351c81a10
Martin Pekár <32843490+MrPekar98@users.noreply.github.com>
2019-05-11 17:31:07 +0200

    Added timeout feature of communication.

--- src/server.c
+++ src/server.c
@@ -1,6 +1,8 @@
 // Server send '1', if command has been terminated successfully, '0' otherwise.
 
 #include <stdio.h>
+#include <pthread.h>
+#include <time.h>
 #include "server.h"
 #include "pi_controller.h"
 #define PORT 58091
@@ -8,7 +10,8 @@
 // Prototypes
 static inline server_con init_sock(unsigned port);
 static inline int connect_client(int sockfd);
-void exec_read(int client_sock);
+static void exec_read(int client_sock);
+void *timer(void *p);
 
 // Main function
 int main()
@@ -20,10 +23,7 @@ int main()
     server_con socket = init_sock(PORT);
 
     if (socket.error)
-    {
-      socket.error = 0;
       continue;
-    }
 
     printf("Connecting...\n");
     int client_fd = connect_client(socket.sockfd);
@@ -46,8 +46,8 @@ int main()
 // Initialises socket.
 static inline server_con init_sock(unsigned port)
 {
-  server_con con;
-  con.sockfd = socket(AF_INET, SOCK_STREAM, 0);;
+  server_con con = {.error = 0};
+  con.sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
   if (con.sockfd < 0)
   {
@@ -77,20 +77,39 @@ static inline int connect_client(int sockfd)
 }
 
 // Reads from client and executed command. Returns when an error occurs.
-void exec_read(int client_sock)
+static void exec_read(int client_sock)
 {
   char buffer[DATA_LEN + 10], ans[2];
   int error = 0;
+  long timeout = 0;
+  pthread_t thread;
+  printf("ID\tMessage\n\n");
+  pthread_create(&thread, NULL, timer, &timeout);
 
   do
   {
     error = read(client_sock, buffer, DATA_LEN + 10);
 
-    if (error < 0)
-      return;
+    if (error < 0 || strlen(buffer) <= 1)
+      continue;
 
+    printf("%d\t%s\n", client_sock, buffer);
     ans[0] = (char) pi_menu(parse_picommand(buffer)) + TO_NUM;
     error = write(client_sock, ans, 18);
   }
-  while (error >= 0);
+  while (error >= 0 && timeout <= 60);
+
+  pthread_cancel(thread);
+}
+
+// Increments time value in seconds.
+void *timer(void *p)
+{
+  time_t start = time(NULL);
+
+  while (1)
+  {
+    long t = time(NULL) - start;
+    p = (void *) &t;
+  }
 }
